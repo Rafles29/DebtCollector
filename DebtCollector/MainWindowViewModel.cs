453459f0a4d92057403ed1174d7e3063c4173a68
Rafles29 <rwozniak@intelgraf.com.pl>
2018-11-21 13:32:05 +0100

    init

---
+++ DebtCollector/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using DebtCollector.Debts;
+using DebtCollector.People;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebtCollector
+{
+    class MainWindowViewModel :BindableBase
+    {
+        private DebtListViewModel _debtListViewModel = new DebtListViewModel();
+        private PersonListViewModel _personListViewModel = new PersonListViewModel();
+
+        private BindableBase _currentViewModel;
+
+        public MainWindowViewModel()
+        {
+            NavCommand = new RelayCommand<string>(OnNav);
+        }
+
+        public BindableBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set { SetProperty(ref _currentViewModel, value); }
+        }
+
+        public RelayCommand<string> NavCommand { get; private set; }
+
+        private void OnNav(string destination)
+        {
+            switch (destination)
+            {
+                case "people":
+                    CurrentViewModel = _personListViewModel;
+                    break;
+                case "debts":
+                    CurrentViewModel = _debtListViewModel;
+                    break;
+            }
+        }
+
+    }
+}
